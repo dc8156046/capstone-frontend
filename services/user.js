6e17754863ca42435e2a48e56eb108bd0f1f42c3
Dan Chen <dan.chen01@edu.sait.ca>
2025-01-20 14:01:48 -0700

    fixed userAPI missing a /

--- services/user.js
+++ services/user.js
@@ -4,7 +4,7 @@ import { apiService } from "./http"; // Import the apiService from services/http
 export const userAPI = {
   login: (email, password) =>
     apiService.postForm("/auth/token", { username: email, password: password }),
-  getUsers: () => apiService.get("/users"),
+  getUsers: () => apiService.get("/users/"),
   addUser: (data) => apiService.post("/users/", data),
   updateUser: (id, data) => apiService.put(`/users/${id}`, data),
   deleteUser: (id) => apiService.delete(`/users/${id}`),
