23fa08c01cb94f41fc3de0c65fa46c039d9659a7
Tianzi Cui <cuitianzi0@gmail.com>
2025-02-11 15:58:35 -0700

    update logo and its size

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
-    <div className="flex items-center gap-2">
-      <div className="w-8 h-8 relative">
+    <div className="flex items-center ">
+      <div className="size-14">
         <img src="/myLogo.png" alt="My Logo" />
       </div>
       <span className="font-bold text-xl">BrickByClick</span>
