eaf20ed6e231f11c6e60e2c9314d20fee7bd951a
ClaraaaL <babananacream@gmail.com>
2025-03-04 14:58:53 -0700

    category_budget_and_process

--- app/dashboard/analytics/page.js
+++ app/dashboard/analytics/page.js
@@ -1,3 +1,12 @@
+import BiaxialLineChart from '@/components/category_budget.js';
+import DateChart from '@/components/category_date.js';
+
 export default function AnalyticsPage() {
-  return <div>AnalyticsPage</div>;
+  return <div>AnalyticsPage
+    <h1>Category_budget</h1>
+    <BiaxialLineChart />
+    <h1>Category_date</h1>
+    <DateChart />
+  </div>
+  ;
 }
--- app/dashboard/project/page.js
+++ app/dashboard/project/page.js
@@ -1,7 +1,13 @@
+import BiaxialLineChart from '@/components/category_budget.js';
+import React from 'react';
+
+
 export default function ProjectPage() {
   return (
     <div>
       <div>ProjectPage</div>
+      
     </div>
   );
 }
+
