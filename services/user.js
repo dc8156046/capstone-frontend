5bd8212f97a4da37a178c3e864feac352180b351
Dan Chen <dan.chen01@edu.sait.ca>
2025-03-25 15:19:45 -0600

    update get all users to post method

--- services/user.js
+++ services/user.js
@@ -4,22 +4,26 @@ import { apiService } from "./http"; // Import the apiService from services/http
 export const userAPI = {
   login: (email, password) =>
     apiService.postForm("/auth/token", { username: email, password: password }),
-  getUsers: () => apiService.get("/users/"),
+  getUsers: () => apiService.post("/users/all"),
   addUser: (data) => apiService.post("/users/", data),
   updateUser: (id, data) => apiService.put(`/users/${id}`, data),
   deleteUser: (id) => apiService.delete(`/users/${id}`),
-  signUp: (email, password, companyName) => 
-    apiService.post("/auth/signup", { email: email, password: password, company: companyName}),
-  activateAccount: (token) => 
-    apiService.post("/auth/activate-account", {token: token}),
+  signUp: (email, password, companyName) =>
+    apiService.post("/auth/signup", {
+      email: email,
+      password: password,
+      company: companyName,
+    }),
+  activateAccount: (token) =>
+    apiService.post("/auth/activate-account", { token: token }),
   forgotPassword: (email) =>
-    apiService.post("/auth/forget-password", { email: email}),
-  verifyCode: (code) => 
-    apiService.post("/auth/verify-code?code=" + code),
+    apiService.post("/auth/forget-password", { email: email }),
+  verifyCode: (code) => apiService.post("/auth/verify-code?code=" + code),
   resetPassword: (email, password) =>
-    apiService.post("/auth/reset-password", {email: email, password: password}),
-  getContractorProjects: () =>
-    apiService.post("/projects/contractor"),
-  getAllProjects: () =>
-    apiService.post("/projects/all"),
+    apiService.post("/auth/reset-password", {
+      email: email,
+      password: password,
+    }),
+  getContractorProjects: () => apiService.post("/projects/contractor"),
+  getAllProjects: () => apiService.post("/projects/all"),
 };
