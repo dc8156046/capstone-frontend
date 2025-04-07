43f0df827c20469efe6e43fd13c86d6b4b1a3ece
Tianzi Cui <cuitianzi0@gmail.com>
2025-04-06 21:55:14 -0600

    add Link to project detail page on project list

--- app/dashboard/project/page.js
+++ app/dashboard/project/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { userAPI } from "@/services";
+import Link from "next/link";
 
 export default function ProjectListPage() {
   const [projects, setProjects] = useState([]);
@@ -47,34 +48,35 @@ export default function ProjectListPage() {
         <div className="text-center mt-4">Loading...</div>
       ) : (
         projects.map((project) => (
-          <div
-            key={project.id}
-            className="grid grid-cols-6 gap-4 p-3 text-center border-b border-gray-200 hover:bg-gray-50"
-          >
-            <div>{project.name}</div>
-            <div>{project.address}</div>
-            <div>
-              {project.start_date ? project.start_date.split("T")[0] : ""}
+          <Link href={`project/${project.id}`} key={project.id}>
+            <div className="grid grid-cols-6 gap-4 p-3 text-center border-b border-gray-200 hover:bg-gray-50">
+              <div>{project.name}</div>
+              <div>{project.address}</div>
+              <div>
+                {project.start_date ? project.start_date.split("T")[0] : ""}
+              </div>
+              <div>
+                {project.end_date ? project.end_date.split("T")[0] : ""}
+              </div>
+              <div>${project.budget}</div>
+              <div
+                className={`text-sm font-semibold px-2 py-1 rounded-full w-fit mx-auto ${
+                  project.status === "completed"
+                    ? "text-green-600"
+                    : project.status === "delayed"
+                    ? "text-red-600"
+                    : project.status === "pending"
+                    ? "text-gray-600"
+                    : project.status === "in_progress"
+                    ? "text-cyan-600"
+                    : ""
+                }`}
+              >
+                {/* {project.status} */}
+                {statusDisplayMap[project.status] || project.status}
+              </div>
             </div>
-            <div>{project.end_date ? project.end_date.split("T")[0] : ""}</div>
-            <div>${project.budget}</div>
-            <div
-              className={`text-sm font-semibold px-2 py-1 rounded-full w-fit mx-auto ${
-                project.status === "completed"
-                  ? "text-green-600"
-                  : project.status === "delayed"
-                  ? "text-red-600"
-                  : project.status === "pending"
-                  ? "text-gray-600"
-                  : project.status === "in_progress"
-                  ? "text-cyan-600"
-                  : ""
-              }`}
-            >
-              {/* {project.status} */}
-              {statusDisplayMap[project.status] || project.status}
-            </div>
-          </div>
+          </Link>
         ))
       )}
     </div>
